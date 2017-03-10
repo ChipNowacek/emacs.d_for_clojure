e498dc0926e6e1588382811a63acca050dd6318c
ChipNowacek <chip@twostewards.com>
2017-03-10 10:11:30 -0500

    installed better-defaults
    Let’s hope they are

--- init.el
+++ init.el
@@ -7,7 +7,7 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- '(package-selected-packages (quote (exec-path-from-shell))))
+ '(package-selected-packages (quote (better-defaults exec-path-from-shell))))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
